831afe6d0fa5f6779a29eb1028a3a957c977b3ae
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-02-06 13:29:21 +0530

    Project initial state complete

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 import userRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imageRoutes.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
 app.listen(PORT, () => {
   console.log("server up!");
 });
